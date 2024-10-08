35b15085e62137f3f2be5688ee063db14cc5ec25
Carol Neves <nevescaroline640@gmail.com>
2024-10-08 19:57:38 -0300

    primeira versão

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+const app = express()
+app.use(express.json())
+
+
+
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+            password: req.body.password
+
+
+        }
+    })
+
+
+
+    res.status(201).json(req.body)
+})
+
+
+app.get('/pegartodos', async (req, res) => {  /* quando acessar (/usuarios) no get cai aqui*/
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(req.body)
+})
+
+app.listen(3000)
+
+
+/* usuario: rodolfo
+senha: jzO03uoF9RZMCKs7*/
