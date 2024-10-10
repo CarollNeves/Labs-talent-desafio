506e97b084a3eaf9b5366e8131ecf94b622d3b67
Carol Neves <nevescaroline640@gmail.com>
2024-10-10 19:24:37 -0300

    Adicionei Endpoint para editar e deletar usuário

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 // Variável global para a URL do banco de dados NeonDB
 
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://neondb_owner:gc2Av1qFbHjY@ep-round-tooth-a8qctxnh.eastus2.azure.neon.tech/neondb?sslmode=require';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://neondb_owner:TjQ6faFrbAy3@ep-lingering-recipe-a5t1jr0b.us-east-2.aws.neon.tech/neondb?sslmode=require';
 const app = express();
 app.use(express.json());  // Endpoint para criar um novo usuário
 
@@ -40,9 +40,59 @@ app.get('/pegartodos', async (req, res) => {
     try {
         const users = await prisma.user.findMany();
         res.status(200).json(users);
+
     } catch (error) {
         console.error('Erro ao pegar usuários:', error);
         res.status(500).json({ error: 'Erro ao pegar usuários' });
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
+app.put('/editarusuarios/:id', async (req, res) => {
+
+    const prisma = new PrismaClient({
+        datasources: {
+            db: { url: DATABASE_URL },
+        },
+    });
+    try {
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: parseInt(req.params.id),
+            },
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password
+            },
+        });
+        res.status(201).json(req.body);
+
+    } catch (error) {
+        console.error('Erro ao editar usuário:', error);
+        res.status(500).json({ error: 'Erro ao editar usuário' });
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+app.delete('/deletarusuarios/:id', async (req, res) => {
+
+    const prisma = new PrismaClient({
+        datasources: {
+            db: { url: DATABASE_URL },
+        },
+    });
+    try {
+        const updatedUser = await prisma.user.delete({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        });
+        res.status(201).json(req.body);
+    } catch (error) {
+        console.error('Erro ao deletar usuário:', error);
+        res.status(500).json({ error: 'Erro ao deletar usuário' });
     } finally {
         await prisma.$disconnect();
     }
