95f9488b4db8bad7aa3219b76b2d58a1bd738fcd
Carol Neves <nevescaroline640@gmail.com>
2024-10-18 15:13:53 -0300

    Simplifiquei o endpoint de login, coloquei as configurações de cors na parte de cima do código

--- server.js
+++ server.js
@@ -3,44 +3,45 @@ import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+
 dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://labstalentsbd_user:X4qfnKBCHejdFpnDdmmoP8TtuJYyo2Y6@dpg-cs4rv5q3esus73alfgng-a.oregon-postgres.render.com/labstalentsbd';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.json());  
+app.use(express.json());
+
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
 
 const generateToken = (userId) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET || 'secret', { expiresIn: '1h' });
 };
 
+// Endpoint de criação de usuários
 app.post('/usuarios', async (req, res) => {
-    const prisma = new PrismaClient({
-        datasources: {
-            db: { url: DATABASE_URL },
-        },
-    });
+    const prisma = new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
+
     try {
         console.log('Dados recebidos:', req.body);
-        
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                email: req.body.email,
-            },
-        });
 
+        const existingUser = await prisma.user.findUnique({ where: { email: req.body.email } });
         if (existingUser) {
             return res.status(400).json({ error: 'Email já cadastrado' });
         }
 
         const newUser = await prisma.user.create({
-            data: {
-                email: req.body.email,
-                name: req.body.name,
-                password: req.body.password,
-            },
+            data: { email: req.body.email, name: req.body.name, password: req.body.password },
         });
-        
+
         res.status(201).json({ message: 'Usuário criado com sucesso', user: newUser });
     } catch (error) {
         console.error('Erro ao criar usuário:', error);
@@ -50,66 +51,36 @@ app.post('/usuarios', async (req, res) => {
     }
 });
 
+// Endpoint de login
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-
-    if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
-        return res.status(400).json({ error: 'Invalid Email format' });
-    }
-
-    if (!password || password.length < 6) { 
-        return res.status(400).json({ error: 'Invalid Password format' });
-    }
-
-    const prisma = new PrismaClient({
-        datasources: {
-            db: { url: DATABASE_URL },
-        },
-    });
+    const prisma = new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
 
     try {
-        const user = await prisma.user.findUnique({
-            where: {
-                email: email,
-            },
-        });
-        if (!user) {
-            return res.status(404).json({ error: 'Invalid email' });
-        }
-
-        if (user.password !== password) {
-            return res.status(404).json({ error: 'Invalid password' });
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user || user.password !== password) {
+            return res.status(404).json({ error: 'Email ou senha inválidos' });
         }
 
         const token = generateToken(user.id);
-
-        return res.status(200).json({ message: 'Login feito com sucesso', token: token });
+        res.status(200).json({ message: 'Login feito com sucesso', token });
     } catch (error) {
         console.error('Erro ao fazer login:', error);
-        return res.status(500).json({ error: 'Erro ao fazer login' });
+        res.status(500).json({ error: 'Erro ao fazer login' });
     } finally {
         await prisma.$disconnect();
     }
 });
 
+// Endpoint para pegar todos os usuários
 app.get('/pegartodos', async (req, res) => {
-    const prisma = new PrismaClient({
-        datasources: {
-            db: { url: DATABASE_URL },
-        },
-    });
+    const prisma = new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
+
     try {
         const users = await prisma.user.findMany({
-            select: {
-              id: true,
-              name: true,
-              email: true,
-              password: true,
-            },
-          });
-          
+            select: { id: true, name: true, email: true, password: true },
+        });
         res.status(200).json(users);
-
     } catch (error) {
         console.error('Erro ao pegar usuários:', error);
         res.status(500).json({ error: 'Erro ao pegar usuários' });
@@ -118,25 +89,16 @@ app.get('/pegartodos', async (req, res) => {
     }
 });
 
+// Endppint para editar usuário
 app.put('/editarusuarios/:id', async (req, res) => {
-    const prisma = new PrismaClient({
-        datasources: {
-            db: { url: DATABASE_URL },
-        },
-    });
+    const prisma = new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
+
     try {
         const updatedUser = await prisma.user.update({
-            where: {
-                id: parseInt(req.params.id),
-            },
-            data: {
-                email: req.body.email,
-                name: req.body.name,
-                password: req.body.password
-            },
+            where: { id: parseInt(req.params.id) },
+            data: { email: req.body.email, name: req.body.name, password: req.body.password },
         });
-        res.status(201).json(req.body);
-
+        res.status(201).json(updatedUser);
     } catch (error) {
         console.error('Erro ao editar usuário:', error);
         res.status(500).json({ error: 'Erro ao editar usuário' });
@@ -145,19 +107,13 @@ app.put('/editarusuarios/:id', async (req, res) => {
     }
 });
 
+// Endpoint para deletar usuário
 app.delete('/deletarusuarios/:id', async (req, res) => {
-    const prisma = new PrismaClient({
-        datasources: {
-            db: { url: DATABASE_URL },
-        },
-    });
+    const prisma = new PrismaClient({ datasources: { db: { url: DATABASE_URL } } });
+
     try {
-        const updatedUser = await prisma.user.delete({
-            where: {
-                id: parseInt(req.params.id),
-            },
-        });
-        res.status(201).json(req.body);
+        await prisma.user.delete({ where: { id: parseInt(req.params.id) } });
+        res.status(200).json({ message: 'Usuário deletado com sucesso' });
     } catch (error) {
         console.error('Erro ao deletar usuário:', error);
         res.status(500).json({ error: 'Erro ao deletar usuário' });
@@ -166,16 +122,7 @@ app.delete('/deletarusuarios/:id', async (req, res) => {
     }
 });
 
-const corsOptions = {
-    origin: 'http://localhost:5173', 
-    credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
-app.use(cors(corsOptions));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
