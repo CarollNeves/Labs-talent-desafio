06e319724e33dacfa40becba042d8267a20fa647
Carol Neves <nevescaroline640@gmail.com>
2024-10-18 15:45:07 -0300

    Mudei a porta do servidor

--- server.js
+++ server.js
@@ -123,6 +123,9 @@ app.delete('/deletarusuarios/:id', async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
+
+
