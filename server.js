03518dd372bd96fb932323a98f5e04eacf742feb
Carol Neves <nevescaroline640@gmail.com>
2024-10-21 15:01:22 -0300

    liberei o cors para todas as origens

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
